05ef1464496cd5e34a63d172c729d8402d677e76
steffanycobos <104925594+steffanycobos@users.noreply.github.com>
2023-02-21 15:16:17 -0500

    Update app.js

--- express/src/app.js
+++ express/src/app.js
@@ -1,24 +1,25 @@
 import express from "express";
-import productos from "./data.js";
 import ProductManager from "./productManager.js";
 
 const app = express();
+let manager = new ProductManager("./dataproductos.json");
 app.get("/", (req, res) => {
+
   res.send("<h1>Hola!</h1>");
 });
-app.get("/products", (req, res) => {
-  res.send(productos);
+app.get("/products",async  (req, res) => {
+  res.send(manager.getProducts());
 });
 
-let manager = new ProductManager();
 app.get("/products/:id", async (req, res) => {
   let num = parseInt(req.params.id);
   res.send(manager.getProductById(num));
 });
 
-app.get("/products", (req, res) => {
+ app.get("/products",async  (req, res) => {
+  const product= await manager.getProducts()
  let { limit } = req.query;
-  res.send(productos.filter((x) => x.id < limit));
+  res.send(product.filter((x) => x.id < limit));
 });
 
 app.listen(8080, () => console.log(`Server listening to port 8080`));
