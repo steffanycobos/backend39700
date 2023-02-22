c3ab74ac5b62fac3c7c4528b103ecb0a2c0887cb
steffany cobos <cobosleandra2@gmail.com>
2023-02-21 19:52:35 -0500

    3er Desafio

--- src/app.js
+++ src/app.js
@@ -3,14 +3,26 @@ import ProductManager from "./productManager.js";
 
 const app = express();
 let manager = new ProductManager("./dataproductos.json");
+
+
 app.get("/", (req, res) => {
   res.send("<h1>Hola!</h1>");
 });
 
-app.get("/products",async  (req, res) => {
-  const product= await manager.getProducts()
-  console.log(product)
-  res.send(product);
+app.get("/products", async (req, res) => {
+  try {
+    const products = await manager.getProducts();
+    const { limit } = req.query;
+
+    if (limit) {
+      products.length = limit;
+      return res.send(products);
+    } else {
+      res.send(products);
+    }
+  } catch (e) {
+    res.status(404).send(`${e}`);
+  }
 });
 
 app.get("/products/:id", async (req, res) => {
@@ -18,9 +30,9 @@ app.get("/products/:id", async (req, res) => {
   res.send(manager.getProductById(num));
 });
 
- app.get("/products",async  (req, res) => {
-  const product= await manager.getProducts()
- let { limit } = req.query;
+app.get("/products", async (req, res) => {
+  const product = await manager.getProducts();
+  let { limit } = req.query;
   res.send(product.filter((x) => x.id < limit));
 });
 
